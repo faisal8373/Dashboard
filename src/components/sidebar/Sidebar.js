11873872c9d0031d190739791c2073af0f70d6c3
faisal8373 <faisal205.vu@gmail.com>
2022-05-07 17:16:42 +0500

    Basic Structure

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Logo from '../../imgs/logo.png'
+import './Sidebar.css'
+import {UilEstate} from '@iconscout/react-unicons'
+function Sidebar() {
+  return (
+    <div className='Sidebar'>
+        <div className='logo'>
+            <img src={Logo} alt='/' />
+            <span>
+                Sh<span>o</span>ps
+            </span>
+        </div>
+        <div className='menu'>
+            <div className='menuItem'>
+                <div>
+                    <UilEstate />
+                </div>
+                <span>Dashboard</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
